dd8becae50b62340af0b922d1c0c10f9076bc86b
Isidro <ffabianiant12@gmail.com>
2024-02-10 23:03:21 -0700

    T: V 0.9.5 Actualizar una tarea, busqueda por id, eliminacion por id completo, asi como el manejo de errores (puedo que falte uno que  otro error por cubrir)

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ let task = [];
 
 // Rutas de las acciones
 app.post('/task',(req, res) => {
-    console.log("Request: "+req);
-    console.log("Response: "+res);
+    //Comprobar que el body tenga title y description
+    if (!req.body.title || !req.body.description){
+        return res.status(400).json({Error: "Es necesario un title o description en el body"});
+    }
 
     // Desestructurar los valores de tittle y description del body
     // de la peticion
@@ -31,13 +33,69 @@ app.post('/task',(req, res) => {
     task.push(newTask);
 
     // Devolver el estado de la respuesta
-    res.status(201).json(newTask);   
+    return res.status(201).json(newTask);   
 });
 
 // Consulta de todas las tareas
 app.get('/task', (req, res) => {
-    res.json(task);
+    
+    // Comprobar si la lista esta vacia
+    if (task.length === 0){
+        return res.status(404).json({Error: "La lista de tareas esta vacia"});
+    }
+
+    // Enviar la lista de tareas
+    return res.status(302).json(task);
 })
+
+// Consultar una tarea por su id
+app.get('/task/one', (req, res) => {
+    const { id } = req.body;
+    
+    // Error  si no encuentra la tarea buscada
+    if (!task.includes(task[id - 1])){
+        return res.status(404).json({Error: "Tarea no encontrada"});
+    }
+
+    return res.status(302).json(task[id - 1]);    
+})
+
+// Eliminar elementos de la lista
+app.delete('/task', (req, res) => {
+    const { id } = req.body
+
+    if (!task.includes(task[id - 1])){
+        return res.status(404).json({Error: "Tarea no encontrada"});
+    }
+
+    delete task[id - 1];
+    return res.status(200).json(task);
+})
+
+// Actualizar la lista de tareas
+app.put('/task', (req, res) => {
+    // Comprobar que la tarea exista
+    if (!task.includes(task[id - 1])){
+        return res.status(400).json({Error: "Tarea no encontrada"});
+    }
+
+    // Comprobar que la informacion de la tarea a actualizar este completa
+    if (!req.body.id || !req.body.title || !req.body.description || !req.body.completed){
+        return res.status(400).json({Error: "Es necesaria la informacion completa de la tarea a actualizar"});
+    }
+
+    const { id, title, description, completed} = req.body;
+
+    // Crear las tareas con la informacion basica (titulo y descripcion) 
+    const updatedTask = new Task(id, title, description, completed);
+
+    // Actualizar la tarea con la nueva tarea creada
+    task[id - 1] = updatedTask;
+
+    // Devolver el estado de la respuesta y mostar la nueva tarea agregada
+    return res.status(200).json(updatedTask);  
+})
+
 // Ejecutar el servidor en el puerto indicado (3000)
 app.listen(PORT, () =>{
     console.log(`Servidor en ejecucion ${PORT}`);
